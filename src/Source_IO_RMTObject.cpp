cac73882232e2f53201a5f56435f0b632a1bfc3c
David Sernaque <dsernaque@rp1.com>
2026-08-18 16:11:18 -0700

    map

--- src/Source_IO_RMTObject.cpp
+++ src/Source_IO_RMTObject.cpp
@@ -474,7 +474,7 @@ void IO_RMTOBJECT::Read (ordered_json& jSrc, RMAP::CORE::MODEL* pModel)
    Transform.d3Position[1] = 0 - jSrc["pTransform"]["Position"][2];
    Transform.d3Position[2] =     jSrc["pTransform"]["Position"][1];
 
-   Transform.d4Rotation[0] = 0 - jSrc["pTransform"]["Rotation"][0];
+   Transform.d4Rotation[0] =     jSrc["pTransform"]["Rotation"][0];
    Transform.d4Rotation[1] = 0 - jSrc["pTransform"]["Rotation"][2];
    Transform.d4Rotation[2] =     jSrc["pTransform"]["Rotation"][1];
    Transform.d4Rotation[3] =     jSrc["pTransform"]["Rotation"][3];
